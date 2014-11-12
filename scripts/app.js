0b468725441ed5c4a4bd1ebb66acc0b01bcbd08e
Fabien Fleureau <fabien.fleureau@gmail.com>
2014-11-12 01:58:57 +0100

    Correction timeline twitter.

--- scripts/app.js
+++ scripts/app.js
@@ -23,3 +23,36 @@ fabienfleureauApp.config(function($routeProvider) {
 		controller : 'MainController'
 	});
 });
+
+fabienfleureauApp.directive('twitterTimeline', [function() {
+	return {
+		restrict: 'A',
+		scope: {
+			cssUrl: "@",
+			autoResize: "="
+		},
+		link: function (scope, element, attrs) {
+			$('body').removeAttr('data-twttr-rendered');
+
+			element
+				.attr('id', 'twitter-feed')
+				.attr("width", "100%" || attrs.width)
+				.attr('data-chrome', 'transparent')
+				.attr('data-widget-id', attrs.twitterTimeline)
+				.addClass('twitter-timeline');
+
+			function render() {
+				var body = $('.twitter-timeline').contents().find('body');
+
+				
+			}
+
+			if (!$('#twitter-wjs').length) {
+				$.getScript((/^http:/.test(document.location)?'http':'https') + '://platform.twitter.com/widgets.js', function() {
+					render();
+					$('.twitter-timeline').load(render);
+        		});
+			}
+		}
+	};
+}]);
